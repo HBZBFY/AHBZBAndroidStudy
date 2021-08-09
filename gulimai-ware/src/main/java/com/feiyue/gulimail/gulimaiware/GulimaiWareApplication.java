25f38712aab09fe3280b55f169568d4215ef9084
17520354165 <lzwylw0826xxx>
2021-08-09 21:37:06 +0800

    项目初始构建

---
+++ gulimai-ware/src/main/java/com/feiyue/gulimail/gulimaiware/GulimaiWareApplication.java
@@ -0,0 +1,13 @@
+package com.feiyue.gulimail.gulimaiware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimaiWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimaiWareApplication.class, args);
+	}
+
+}
