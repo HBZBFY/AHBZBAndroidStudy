de62dca6aac80b9d0f6b243e65d4a20a67f31c64
17520354165 <lzwylw0826xxx>
2021-08-10 22:57:52 +0800

    新增mapper,部分配置

--- gulimail-product/src/main/java/com/feiyue/gulimail/product/GulimailProductApplication.java
+++ gulimail-product/src/main/java/com/feiyue/gulimail/product/GulimailProductApplication.java
@@ -2,8 +2,10 @@ package com.feiyue.gulimail.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimailProductApplication {
 
 	public static void main(String[] args) {
