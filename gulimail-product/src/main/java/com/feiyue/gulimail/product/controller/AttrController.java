c616a87cde0becd78c43aa43c9500f1fd4a213a6
jony.liang <jony.liang@hongkutech.com>
2021-08-16 10:04:01 +0800

    添加部分功能

--- gulimail-product/src/main/java/com/feiyue/gulimail/product/controller/AttrController.java
+++ gulimail-product/src/main/java/com/feiyue/gulimail/product/controller/AttrController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 
 
+import com.feiyue.common.utils.R;
+import com.feiyue.gulimail.product.service.AttrService;
+import com.feiyue.gulimail.product.vo.AttrVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,5 +18,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("product/attr")
 public class AttrController {
 
+    @Autowired
+    private AttrService attrService;
 
+    @RequestMapping("save")
+    public R save(@RequestBody AttrVo attrVo) {
+        attrService.saveVo(attrVo);
+        return R.ok();
+    }
 }
--- gulimail-product/src/main/java/com/feiyue/gulimail/product/service/impl/AttrServiceImpl.java
+++ gulimail-product/src/main/java/com/feiyue/gulimail/product/service/impl/AttrServiceImpl.java
@@ -2,9 +2,13 @@ package com.feiyue.gulimail.product.service.impl;
 
 
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.feiyue.gulimail.product.dao.AttrAttrgroupRelationDao;
 import com.feiyue.gulimail.product.dao.AttrDao;
+import com.feiyue.gulimail.product.entity.AttrAttrgroupRelationEntity;
 import com.feiyue.gulimail.product.entity.AttrEntity;
+import com.feiyue.gulimail.product.service.AttrAttrgroupRelationService;
 import com.feiyue.gulimail.product.service.AttrService;
+import com.feiyue.gulimail.product.vo.AttrVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -25,5 +30,15 @@ import org.springframework.util.StringUtils;
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
 
-
+    @Autowired
+    private AttrAttrgroupRelationDao attrAttrgroupRelationDao;
+    @Override
+    public void saveVo(AttrVo attrVo) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attrVo, attrEntity);
+        this.save(attrEntity);
+        AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = new AttrAttrgroupRelationEntity();
+        BeanUtils.copyProperties(attrVo, attrAttrgroupRelationEntity);
+        attrAttrgroupRelationDao.insert(attrAttrgroupRelationEntity);
+    }
 }
