cd080f62035792dfa31e278c4f42d2ed470f734b
jony.liang <jony.liang@hongkutech.com>
2021-08-17 17:08:45 +0800

    添加部分新功能

---
+++ gulimail-product/src/main/java/com/feiyue/gulimail/product/feign/CouponFeignService.java
@@ -0,0 +1,19 @@
+package com.feiyue.gulimail.product.feign;
+
+import com.feiyue.common.to.SkuReductionTo;
+import com.feiyue.common.to.SpuBoundTo;
+import com.feiyue.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @PostMapping("/coupon/spubounds/save")
+    R saveSpuBounds(@RequestBody SpuBoundTo spuBoundTo);
+
+
+    @PostMapping("/coupon/skufullreduction/saveinfo")
+    R saveSkuReduction(@RequestBody SkuReductionTo skuReductionTo);
+}
--- gulimail-product/src/main/java/com/feiyue/gulimail/product/service/impl/SpuInfoServiceImpl.java
+++ gulimail-product/src/main/java/com/feiyue/gulimail/product/service/impl/SpuInfoServiceImpl.java
@@ -1,18 +1,17 @@
 package com.feiyue.gulimail.product.service.impl;
 
 
+import com.feiyue.common.to.SkuReductionTo;
+import com.feiyue.common.to.SpuBoundTo;
+import com.feiyue.common.utils.R;
 import com.feiyue.gulimail.product.dao.AttrDao;
 import com.feiyue.gulimail.product.dao.SpuImagesDao;
 import com.feiyue.gulimail.product.dao.SpuInfoDao;
 import com.feiyue.gulimail.product.dao.SpuInfoDescDao;
 import com.feiyue.gulimail.product.entity.*;
-import com.feiyue.gulimail.product.service.ProductAttrValueService;
-import com.feiyue.gulimail.product.service.SpuImagesService;
-import com.feiyue.gulimail.product.service.SpuInfoDescService;
-import com.feiyue.gulimail.product.service.SpuInfoService;
-import com.feiyue.gulimail.product.vo.BaseAttrs;
-import com.feiyue.gulimail.product.vo.Bounds;
-import com.feiyue.gulimail.product.vo.SpuSaveVo;
+import com.feiyue.gulimail.product.feign.CouponFeignService;
+import com.feiyue.gulimail.product.service.*;
+import com.feiyue.gulimail.product.vo.*;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -46,6 +45,18 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
 
     @Autowired
     private AttrDao attrDao;
+
+    @Autowired
+    private CouponFeignService couponFeignService;
+
+    @Autowired
+    private SkuInfoService skuInfoService;
+
+    @Autowired
+    private SkuImagesService skuImagesService;
+
+    @Autowired
+    private SkuSaleAttrValueService skuSaleAttrValueService;
 
     @Override
     public void saveSpuInfo(SpuSaveVo vo) {
@@ -83,6 +94,74 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
 
         // 保存积分信息
         Bounds bounds = vo.getBounds();
+        SpuBoundTo spuBoundTo = new SpuBoundTo();
+        BeanUtils.copyProperties(bounds,spuBoundTo);
+        spuBoundTo.setSpuId(spuInfoEntity.getId());
+        R r = couponFeignService.saveSpuBounds(spuBoundTo);
+        if(r.getCode() != 0){
+            log.error("远程保存spu积分信息失败");
+        }
 
+        // 保存spu对应的sku
+        List<Skus> skus = vo.getSkus();
+        if (skus != null && !skus.isEmpty()) {
+            skus.forEach(item -> {
+                String defaultImg = "";
+                List<Images> images1 = item.getImages();
+                if (images1 != null && !images1.isEmpty()) {
+                    for (Images images2 : images1) {
+                        if (images2.getDefaultImg() == 1) {
+                            defaultImg = images2.getImgUrl();
+                        }
+                    }
+                }
+                SkuInfoEntity skuInfoEntity = new SkuInfoEntity();
+                BeanUtils.copyProperties(item,skuInfoEntity);
+                skuInfoEntity.setBrandId(spuInfoEntity.getBrandId());
+                skuInfoEntity.setCatalogId(spuInfoEntity.getCatalogId());
+                skuInfoEntity.setSaleCount(0L);
+                skuInfoEntity.setSpuId(spuInfoEntity.getId());
+                skuInfoEntity.setSkuDefaultImg(defaultImg);
+                //5.1）、sku的基本信息；pms_sku_info
+                skuInfoService.save(skuInfoEntity);
+
+                Long skuId = skuInfoEntity.getSkuId();
+
+                List<SkuImagesEntity> imagesEntities = item.getImages().stream().map(img -> {
+                    SkuImagesEntity skuImagesEntity = new SkuImagesEntity();
+                    skuImagesEntity.setSkuId(skuId);
+                    skuImagesEntity.setImgUrl(img.getImgUrl());
+                    skuImagesEntity.setDefaultImg(img.getDefaultImg());
+                    return skuImagesEntity;
+                }).filter(entity->{
+                    //返回true就是需要，false就是剔除
+                    return !StringUtils.isEmpty(entity.getImgUrl());
+                }).collect(Collectors.toList());
+                //5.2）、sku的图片信息；pms_sku_image
+                skuImagesService.saveBatch(imagesEntities);
+
+                List<Attr> attr = item.getAttr();
+                List<SkuSaleAttrValueEntity> skuSaleAttrValueEntities = attr.stream().map(a -> {
+                    SkuSaleAttrValueEntity attrValueEntity = new SkuSaleAttrValueEntity();
+                    BeanUtils.copyProperties(a, attrValueEntity);
+                    attrValueEntity.setSkuId(skuId);
+
+                    return attrValueEntity;
+                }).collect(Collectors.toList());
+                //5.3）、sku的销售属性信息：pms_sku_sale_attr_value
+                skuSaleAttrValueService.saveBatch(skuSaleAttrValueEntities);
+
+                // //5.4）、sku的优惠、满减等信息；gulimall_sms->sms_sku_ladder\sms_sku_full_reduction\sms_member_price
+                SkuReductionTo skuReductionTo = new SkuReductionTo();
+                BeanUtils.copyProperties(item,skuReductionTo);
+                skuReductionTo.setSkuId(skuId);
+                if(skuReductionTo.getFullCount() >0 || skuReductionTo.getFullPrice().compareTo(new BigDecimal("0")) == 1){
+                    R r1 = couponFeignService.saveSkuReduction(skuReductionTo);
+                    if(r1.getCode() != 0){
+                        log.error("远程保存sku优惠信息失败");
+                    }
+                }
+            });
+        }
     }
 }
